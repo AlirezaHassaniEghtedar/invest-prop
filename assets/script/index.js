9e2c7f0a5c068902331009833810e22b499f3fd3
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-01-19 19:29:13 +0330

    update 01

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,105 +1,125 @@
 const mobileMenuIcon = document.querySelector('header .right-section .mobile-menu-icon')
 const mobileNavMenu = document.querySelector('header nav')
 const mobileNavMenuCloseBtn = document.querySelector('header nav .close-btn')
-
+const plansSectionHeaderBtns = document.querySelectorAll('.plans-section .plans-buttons button')
+const plansSectionSwipers = document.querySelectorAll('.plans-section .swiper-wrapper-container .swiper-container .swiper')
 
 
 // Event Listeners
-mobileMenuIcon.addEventListener('click' , handleMobileMenuIconClick)
-mobileNavMenuCloseBtn.addEventListener('click' , handleMobileMenuIconClick)
+mobileMenuIcon.addEventListener('click', handleMobileMenuIconClick)
+mobileNavMenuCloseBtn.addEventListener('click', handleMobileMenuIconClick)
+
+plansSectionHeaderBtns.forEach(btn => btn.addEventListener('click', handlePlansSectionHeaderBtnClick))
 
 // Functions
-function handleMobileMenuIconClick () {
+function handleMobileMenuIconClick() {
   mobileNavMenu.classList.toggle('active')
+}
+
+function handlePlansSectionHeaderBtnClick(e) {
+  const targetBtn = e.target
+
+  if (targetBtn.classList.contains('active')) return
+
+  plansSectionHeaderBtns.forEach(btn => btn.classList.remove('active'))
+  targetBtn.classList.add('active')
+
+  handleLoadSwiper(targetBtn.getAttribute('swiper-number'))
+}
+
+function handleLoadSwiper(num) {
+  plansSectionSwipers.forEach(item => {
+    Number(item.id) === Number(num) ? item.classList.add('active') : item.classList.remove('active');
+  })
 }
 
 
 
 
+var swiper = new Swiper(".heroSection .mySwiper .swiper-wrapper-container .swiper-container", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".heroSection .swiper-button-next",
+    prevEl: ".heroSection .swiper-button-prev",
+  },
+  pagination: {
+    el: ".heroSection .swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  mousewheel: true,
+  keyboard: true,
+});
 
-var swiper = new Swiper(".heroSection .mySwiper", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".heroSection .swiper-button-next",
-      prevEl: ".heroSection .swiper-button-prev",
-    },
-    pagination: {
-      el: ".heroSection .swiper-pagination",
-    },
-    loop: true,
-    mousewheel: true,
-    keyboard: true,
-  });
+var swiper = new Swiper(".plans-section .mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  cssMode: true,
+  navigation: {
+    nextEl: ".plans-section .swiper-button-next",
+    prevEl: ".plans-section .swiper-button-prev",
+  },
+  pagination: {
+    el: ".plans-section .swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  mousewheel: true,
+  keyboard: true,
 
-  var swiper = new Swiper(".plans-section .mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    cssMode: true,
-    navigation: {
-      nextEl: ".plans-section .swiper-button-next",
-      prevEl: ".plans-section .swiper-button-prev",
+  breakpoints: {
+    570: {
+      slidesPerView: 1,
+      spaceBetween: 15,
     },
-    pagination: {
-      el: ".plans-section .swiper-pagination",
-      clickable: true,
+    767: {
+      slidesPerView: 1,
+      spaceBetween: 15,
     },
-    loop: true,
-    mousewheel: true,
-    keyboard: true,
-
-    breakpoints: {
-      570: {
-        slidesPerView : 1 ,
-        spaceBetween : 15,
-      },
-      767: {
-        slidesPerView : 1 ,
-        spaceBetween : 15,
-      },
-      840: {
-        slidesPerView : 2 ,
-        spaceBetween : 20,
-      },
-      1150: {
-        slidesPerView : 3 ,
-        spaceBetween : 25,
-      },
-    }
-  });
+    840: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1150: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+  }
+});
 
 
-  var swiper = new Swiper(".videos-section .mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    cssMode: true,
-    navigation: {
-      nextEl: ".videos-section .swiper-button-next",
-      prevEl: ".videos-section .swiper-button-prev",
-    },
-    pagination: {
-      el: ".videos-section .swiper-pagination",
-      clickable: true,
-    },
-    loop: true,
-    mousewheel: true,
-    keyboard: true,
+var swiper = new Swiper(".videos-section .mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  cssMode: true,
+  navigation: {
+    nextEl: ".videos-section .swiper-button-next",
+    prevEl: ".videos-section .swiper-button-prev",
+  },
+  pagination: {
+    el: ".videos-section .swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  mousewheel: true,
+  keyboard: true,
 
-    breakpoints: {
-      570: {
-        slidesPerView : 1 ,
-        spaceBetween : 15,
-      },
-      767: {
-        slidesPerView : 1 ,
-        spaceBetween : 15,
-      },
-      840: {
-        slidesPerView : 2 ,
-        spaceBetween : 20,
-      },
-      1150: {
-        slidesPerView : 3 ,
-        spaceBetween : 25,
-      },
-    }
-  });
+  breakpoints: {
+    570: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    767: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    840: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1150: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+  }
+});
---
+++ assets/script/particle.js
@@ -0,0 +1,178 @@
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#3d8bf2"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#3d8bf2"
+            },
+            "polygon": {
+                "nb_sides": 5
+            }
+        },
+        "opacity": {
+            "value": 0.75,
+            "random": false
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#3d8bf2",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 4,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            }
+        },
+        "modes": {
+            "repulse": {
+                "distance": 200
+            },
+            "push": {
+                "particles_nb": 4
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+// particlesJS('particles-js2', {
+//     "particles": {
+//       "number": {
+//         "value": 100,
+//         "density": {
+//           "enable": true,
+//           "value_area": 800
+//         }
+//       },
+//       "color": {
+//         "value": "#00aaff"
+//       },
+//       "shape": {
+//         "type": "circle"
+//       },
+//       "opacity": {
+//         "value": 0.3,
+//         "random": true
+//       },
+//       "size": {
+//         "value": 10,
+//         "random": true
+//       },
+//       "move": {
+//         "enable": true,
+//         "speed": 1.75,
+//         "direction": "top",
+//         "random": true,
+//         "straight": false,
+//         "out_mode": "out",
+//         "bounce": false
+//       }
+//     },
+//     "retina_detect": true
+//   });
+  
+
+// particlesJS('particles-js2', {
+//     "particles": {
+//         "number": {
+//             "value": 100,
+//             "density": {
+//                 "enable": true,
+//                 "value_area": 800
+//             }
+//         },
+//         "color": {
+//             "value": "#3d8bf2"
+//         },
+//         "shape": {
+//             "type": "circle",
+//             "stroke": {
+//                 "width": 0,
+//                 "color": "#3d8bf2"
+//             },
+//             "polygon": {
+//                 "nb_sides": 5
+//             }
+//         },
+//         "opacity": {
+//             "value": 0.75,
+//             "random": false
+//         },
+//         "size": {
+//             "value": 3,
+//             "random": true
+//         },
+//         "line_linked": {
+//             "enable": true,
+//             "distance": 150,
+//             "color": "#3d8bf2",
+//             "opacity": 0.4,
+//             "width": 1
+//         },
+//         "move": {
+//             "enable": true,
+//             "speed": 5,
+//             "direction": "none",
+//             "random": false,
+//             "straight": false,
+//             "out_mode": "out",
+//             "bounce": false
+//         }
+//     },
+//     "interactivity": {
+//         "detect_on": "canvas",
+//         "events": {
+//             "onhover": {
+//                 "enable": true,
+//                 "mode": "repulse"
+//             },
+//             "onclick": {
+//                 "enable": true,
+//                 "mode": "push"
+//             }
+//         },
+//         "modes": {
+//             "repulse": {
+//                 "distance": 200
+//             },
+//             "push": {
+//                 "particles_nb": 4
+//             }
+//         }
+//     },
+//     "retina_detect": true
+// });
